b32c9417e74f9a5876e8980ae91ee158148f2a84
unknown <s1200522@WS1200522.bc.local>
2013-10-29 18:31:15 +0200

    uuden "drawing shapes" tutoriaalin alotus

--- Opengles/src/com/example/opengles/MyRenderer.java
+++ Opengles/src/com/example/opengles/MyRenderer.java
@@ -1,5 +1,9 @@
 package com.example.opengles;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.opengles.GL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import android.opengl.GLES20;
@@ -18,4 +22,27 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 	public void onSurfaceChanged(GL10 unused, int width, int height) {
 		GLES20.glViewport(0, 0, width, height);
 	}
+}
+
+class Triangle {
+	private FloatBuffer vertexBuffer;
+	
+	static final int COORDS_PER_VERTEX = 3;
+	static float triangleCoords[] = {
+		0.0f, -0.5f, 0.0f, //
+		0.5f, 0.0f, 0.0f, // oikea piste
+		-0.5f, 0.0f, 0.0f // vasen piste
+	};
+	float color[] = { 1.0f, 0.98f, 0.0f, 1.0f}; // R, G, B, A = läpinäkyvyys. f = float eli desimaali
+	
+	public Triangle() {
+		ByteBuffer bb = ByteBuffer.allocateDirect (triangleCoords.length * 4);
+		
+		bb.order(ByteOrder.nativeOrder());
+		
+		vertexBuffer.put(triangleCoords);
+		
+		vertexBuffer.position(0);
+	}
+	
 }
